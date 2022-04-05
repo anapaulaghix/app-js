1e3ee806381fe29b22de10e63e23b645777a72dc
ana paula <anapaulaghis@gmail.com>
2022-04-05 19:31:38 -0300

    update 05/04

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Home from './components/pages/Home'
 import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
+import Container from './components/layout/Container'
 
 function App() {
   return (
     <Router>
       <div>
         <Link to="/">Home</Link>
-        <Link to="/">Company</Link>
-        <Link to="/">Contact</Link>
+        <Link to="/company">Company</Link>
+        <Link to="/contact">Contact</Link>
       </div>
       <Routes>
-          <Route exact path="/Home">
+        <Container>
+          <Route exact path="/">
             <Home />
           </Route>
           <Route exact path="/Company">
@@ -21,7 +23,9 @@ function App() {
           <Route exact path="/Contact">
             <Contact />
           </Route>
+        </Container>
       </Routes>
+      <p>Footer</p>
     </Router>
   )
 }
