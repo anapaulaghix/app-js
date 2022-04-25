6fabb9c36a83f3b31796b3fec5446ac76a371b4c
anapaulaghix <anapaulaghis@gmail.com>
2022-04-25 16:54:54 -0300

    update

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -7,6 +7,8 @@ function Navbar(){
     return(
         <nav className={styles.navbar}>
             <Container>
+            <Link to="/">
+          <img src={logo} alt="Logoicon" />  </Link>
                 <ul className={styles.list}>
                     <li className={styles.item}>
                     <Link to="/">Home</Link>
