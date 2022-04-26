e1a1ef6cd0e8755c806419f98105e0633c9793a7
anapaulaghix <anapaulaghis@gmail.com>
2022-04-26 09:02:40 -0300

    update

--- src/components/pages/Company.js
+++ src/components/pages/Company.js
@@ -1,5 +1,12 @@
 function Company(){
-    return <h1>Quem Somos</h1>
+    return (
+        <div>
+            <h1>Quem Somos</h1>
+            <p>Somos uma instituição de doações de produtos online, temos o intuito de ajudar sem fins lucrativos. 
+                        O nosso site foi feito para você doar ou receber sem contato pessoal com o doador/receptor.
+                    A nossa ideia é que você selecione somente o que quiser/precisa, para que não haja disperdício.</p>
+        </div>
+    )
 }
 
 export default Company
