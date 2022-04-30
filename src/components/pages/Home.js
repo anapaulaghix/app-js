9cfc989848ecd4fafc2895dbedffb761a93bdc42
anapaulaghix <anapaulaghis@gmail.com>
2022-04-30 13:13:15 -0300

    update

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,10 +6,8 @@ function Home(){
     return (
         <section className={styles.home_container}>
             <img src={template} alt='template'></img>
-            <h1>Bem-vindo ao <span>Solidariti</span></h1>
-            <p>Escolha uma opção e seja um voluntário</p>
-            <LinkButton to="/doar" text="Doar"></LinkButton>
-            <LinkButton to="/receber" text="Receber"></LinkButton>
+            <LinkButton to="/doar" text=" Quero Doar"></LinkButton>
+            <LinkButton to="/receber" text="Quero Receber"></LinkButton>
         </section>
     )
 }
