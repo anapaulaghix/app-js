97f8d5c8f11b47c96c8da7a418d4d09e0e43e255
anapaulaghix <anapaulaghis@gmail.com>
2022-04-30 16:16:10 -0300

    update

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,8 +6,12 @@ function Home(){
     return (
         <section className={styles.home_container}>
             <img src={template} alt='template'></img>
+            <h1>Escolha sua opção</h1>
             <LinkButton to="/doar" text=" Quero Doar"></LinkButton>
             <LinkButton to="/receber" text="Quero Receber"></LinkButton>
+            <p>Lembrando que você não terá contato com seu doador! Somos uma Instuição!</p> 
+            <p>Você pode levar sua doação ou buscar o que escolheu receber na instituição mais próxima de você.</p>
+            <p>Aceitamos alimentos, produtos, etc. Ajude como quiser. </p> 
         </section>
     )
 }
