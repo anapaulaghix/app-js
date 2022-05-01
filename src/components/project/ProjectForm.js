667183fec2a9e104f21eee547fdac5bd43259093
anapaulaghix <anapaulaghis@gmail.com>
2022-05-01 16:36:40 -0300

    update

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -3,6 +3,9 @@ import styles from './ProjectForm.module.css'
 function ProjectForm(){
     return (
         <form> 
+            <select name="category_id">
+                <option>Selecione o tipo de produto</option>
+            </select>
 
         </form>
     )
