c8855ed64225d8f414f4ee3c6829726ba011f1c7
anapaulaghix <anapaulaghis@gmail.com>
2022-05-02 22:42:23 -0300

    update

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,11 +1,17 @@
+import Input from '../form/input'
+import Select from '../form/Select'
 import styles from './ProjectForm.module.css'
 
 function ProjectForm(){
     return (
-        <form> 
-            <select name="category_id">
-                <option>Selecione o tipo de produto</option>
-            </select>
+        <form className={styles.form}> 
+        <Select />
+        <Input
+        type="text"
+        text="Especificação do produto"
+        name="name"
+        placeholder="Especificação do produto. Ex: arroz, feijão, etc."
+         />
             <div>
             <input type='submit' value='Adicionar ao carrinho'></input>
             </div>
